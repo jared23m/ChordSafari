ac855c06b268333d6c345233b7ec3296e3506605
Jared May <jared23m@gmail.com>
2024-06-19 12:58:28 -0500

    combo checklist rendered

--- src/components/ComboSelect.jsx
+++ src/components/ComboSelect.jsx
@@ -1,5 +1,27 @@
-export default function ComboSelect(){
+export default function ComboSelect(props){
+    function renderComboFormSection(combo, index){
+        return (
+            <div key={index}>
+                <label className='comboSelectLabel'>
+                        <input type="checkbox" value={combo} checked={props.combos[combo]} onChange={()=>{
+                            const currentCombos = props.combos;
+                            const currentSingleCombo = currentCombos[combo];
+                            props.setCombos({...currentCombos, [combo]: !currentSingleCombo});
+                        }}/>
+                        {combo}
+                </label>
+            </div>
+            )
+    }
+
     return (
-        <p>ComboSelect</p>
+        <div className="comboelect">
+            <form className="comboSelectForm">
+                {Object.keys(props.combos).map((combo, index) => (
+                renderComboFormSection(combo, index)
+                ))}
+            </form>
+    </div>
     )
+
 }
--- src/musicTheory/index.jsx
+++ src/musicTheory/index.jsx
@@ -20,3 +20,20 @@ export const chordRelativeRoots = [
     "vi",
     "viidim"
 ]
+
+export const comboList = [
+    ['1'],
+    ['11','12'],
+    ['111', '112', '121', '122', '123'],
+    ['1111', '1112', '1121', '1122', '1211', '1212', '1221', '1222', '1123', '1213',  '1223', '1231', '1232', '1233']
+]
+
+export function comboCheckCreator(chordAmount){
+    const correctComboSection = comboList[chordAmount - 1];
+    const comboCheckObject = [];
+    correctComboSection.forEach((combo) =>{
+        comboCheckObject[combo] = false;
+    })
+
+    return comboCheckObject;
+}
